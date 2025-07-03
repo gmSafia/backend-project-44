5b3c5445f63f23003baa1ad508ef8192f07942e5
Safia <safiygalim@gmail.com>
2025-07-03 13:19:44 +0300

    rename file

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,27 @@
-import readlineSync from 'readline-sync'
+import readline from 'readline-sync'
 
-export default function greetUser() {
+export default function runGame(instruction, getRounds) {
   console.log('Welcome to the Brain Games!')
-  const userName = readlineSync.question('May I have your name? ')
+
+  const userName = readline.question('May I have your name? ')
   console.log(`Hello, ${userName}`)
+
+  console.log(instruction)
+
+  const rounds = getRounds()
+
+  for (const [question, correctAnswer] of rounds) {
+    console.log(`Question: ${question}`)
+    const answer = readline.question(`Your answer: `)
+
+    if (answer === String(correctAnswer)) {
+      console.log(`Correct!`)
+    }
+    else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${String(correctAnswer)})\nLet's try again, ${userName}!`)
+      return
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`)
 }
